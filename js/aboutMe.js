788fd41e9957751a042a30582aba32d92e731d80
Camilla Seeland <icami@hotmail.no>
2023-05-18 20:40:26 +0200

    updated table file, and created about me function

---
+++ js/aboutMe.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+function updateAboutMeContent() {
+  $(".about p").text("my hobbies are drawing, gaming and coding! I love pickles and potatochips. I have a fat cat named Arya! I currently live in Norway");
+
+  $(".aboutme").text("I am FJ - the code master");
+}
+
+updateAboutMeContent();
+
+$(".about p").click(function() {
+
+//background must be powder blue when clicked and text is white
+
+}
+)
+
+
+}); 
--- js/table.js
+++ js/table.js
@@ -1,22 +1,29 @@
 let tabledata = [
-    {id: 1, icon: 'icon1', description: 'Description 1', info: 'Info 1', image: 'image1.jpg'},
-    {id: 2, icon: 'icon2', description: 'Description 2', info: 'Info 2', image: 'image2.jpg'},
-    {id: 3, icon: 'icon3', description: 'Description 3', info: 'Info 3', image: 'image3.jpg'},
-    {id: 4, icon: 'icon4', description: 'Description 4', info: 'Info 4', image: 'image4.jpg'},
-    {id: 5, icon: 'icon5', description: 'Description 5', info: 'Info 5', image: 'image5.jpg'},
+    { id: 1, icon: 'bi-globe', description: 'Description 1', info: 'Info 1', image: 'https://cdn.pixabay.com/photo/2018/04/13/16/13/pixel-3316924_960_720.png' },
+    { id: 2, icon: 'bi-chat-dots', description: 'Description 2', info: 'Info 2', image: 'image2.jpg' },
+    { id: 3, icon: 'bi-heart', description: 'Description 3', info: 'Info 3', image: 'image3.jpg' },
+    { id: 4, icon: 'bi-star', description: 'Description 4', info: 'Info 4', image: 'image4.jpg' },
+    { id: 5, icon: 'bi-envelope', description: 'Description 5', info: 'Info 5', image: 'image5.jpg' },
+];
 
-]
-
-  function generateTable(table, data) {
+function generateTable(table, data) {
     for (let element of data) {
       let row = table.insertRow();
       for (key in element) {
         let cell = row.insertCell();
-        let text = document.createTextNode(element[key]);
-        cell.appendChild(text);
+        if (key === 'icon') {
+          let icon = document.createElement('i');
+          icon.className = 'bi ' + element[key];
+          cell.appendChild(icon);
+        } else {
+          let text = document.createTextNode(element[key]);
+          cell.appendChild(text);
+        }
       }
     }
   }
+
+
   
   let table = document.querySelector("table");
-generateTable(table, tabledata);
+  generateTable(table, tabledata);
