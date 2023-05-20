7b9ad012453f3b71db86d09459ea3ece4e86a840
Camilla Seeland <icami@hotmail.no>
2023-05-20 23:12:13 +0200

    removed notes from files

--- js/welcome.js
+++ js/welcome.js
@@ -4,11 +4,6 @@ $(document).ready(function() {
       }); 
     });
 
-    //GET request when clikcin the I"m Bored button
-    //changed to ajax instead of get, as $.ajax provides
-    // more flexibility and options compared to the shorthand method $.get.
-
-    
     $("#boredBtn").click(function() {
       $.ajax({
         url: "https://www.boredapi.com/api/activity/",
